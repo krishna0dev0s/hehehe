cbdff8d370dea1220b14c939c1082956ca4f226c
krishna0dev0s <kg933617@gmail.com>
2025-11-23 00:08:09 +0530

    Fix Turbopack build error: Add empty turbopack config

--- next.config.mjs
+++ next.config.mjs
@@ -12,12 +12,7 @@ const nextConfig = {
       },
     ],
   },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals.push("_http_common");
-    }
-    return config;
-  },
+  turbopack: {},
 };
 
 export default nextConfig;
